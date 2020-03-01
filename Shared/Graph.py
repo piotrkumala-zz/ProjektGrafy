bd6036fbf9b8d7c3c372c76f3cde2057b49c5dc3
Piotr Kumala <piotrek.kumala@gmail.com>
2020-03-01 16:11:12 +0100

    Refactor

--- Shared/Graph.py
+++ Shared/Graph.py
@@ -7,13 +7,13 @@ class Graph(object):
 
     def addEdge(self, v1, v2):
         if v1 == v2:
-            print("Same vertex %d and %d" % (v1, v2))
+            print("Already exists")
         self.adjMatrix[v1][v2] = 1
         self.adjMatrix[v2][v1] = 1
 
     def removeEdge(self, v1, v2):
         if self.adjMatrix[v1][v2] == 0:
-            print("No edge between %d and %d" % (v1, v2))
+            print("Nothing to delete")
             return
         self.adjMatrix[v1][v2] = 0
         self.adjMatrix[v2][v1] = 0
@@ -21,11 +21,8 @@ class Graph(object):
     def containsEdge(self, v1, v2):
         return True if self.adjMatrix[v1][v2] > 0 else False
 
-    def __len__(self):
-        return self.size
-
     def printAdjacencyMatrix(self):
         for row in self.adjMatrix:
             for val in row:
-                print(str(val) + " ", end='')
+                print("%d " % val, end='')
             print("")
