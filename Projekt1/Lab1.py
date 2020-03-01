42f2af9b77bb0da44bd1a67a96116df2891d37a0
Piotr Kumala <piotrek.kumala@gmail.com>
2020-03-01 16:08:01 +0100

    Add class Graph for basic operations on graph, implement AdjacencyMatrix in this class

--- Projekt1/Lab1.py
+++ Projekt1/Lab1.py
@@ -1,1 +1,11 @@
-print("Hello world!")
+from Shared.Graph import Graph
+
+g = Graph(4)
+
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+
+g.printAdjacencyMatrix()
---
+++ Shared/Graph.py
@@ -0,0 +1,31 @@
+class Graph(object):
+    def __init__(self, size):
+        self.adjMatrix = []
+        for i in range(size):
+            self.adjMatrix.append([0 for i in range(size)])
+        self.size = size
+
+    def addEdge(self, v1, v2):
+        if v1 == v2:
+            print("Same vertex %d and %d" % (v1, v2))
+        self.adjMatrix[v1][v2] = 1
+        self.adjMatrix[v2][v1] = 1
+
+    def removeEdge(self, v1, v2):
+        if self.adjMatrix[v1][v2] == 0:
+            print("No edge between %d and %d" % (v1, v2))
+            return
+        self.adjMatrix[v1][v2] = 0
+        self.adjMatrix[v2][v1] = 0
+
+    def containsEdge(self, v1, v2):
+        return True if self.adjMatrix[v1][v2] > 0 else False
+
+    def __len__(self):
+        return self.size
+
+    def printAdjacencyMatrix(self):
+        for row in self.adjMatrix:
+            for val in row:
+                print(str(val) + " ", end='')
+            print("")
